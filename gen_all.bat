5b040c6c923174fd1562465547b06a4812e23be3
wenjingyu <wjyjy1999@outlook.com>
2022-11-07 22:47:00 +0800

    1、更新批处理脚本，实现lib的生成

--- gen_all.bat
+++ gen_all.bat
@@ -1,5 +1,7 @@
-@rem @echo off
-echo gen lib start
-cmd /k "cd ./baselib/build && cmake .."
-
+@echo off
+echo gen all start
+set bat_src=%~dp0
+set vs_path=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build
+cmd /k "cd ./baselib/build && cmake .. && cd %vs_path% && C: && vcvars64.bat && cd %bat_src%/baselib/build && D: && devenv sqlite3.sln /build"
+echo gen all done
 pause
