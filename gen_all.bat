c1d95201760b03511e63fc75f90c1fa0e1d31fbc
wenjingyu <wjyjy1999@outlook.com>
2022-11-08 08:39:31 +0800

    1、更新批处理脚本

--- gen_all.bat
+++ gen_all.bat
@@ -2,6 +2,9 @@
 echo ****Note: gen all start****
 set all_path=%~dp0
 call %all_path%/baselib/gen_lib.bat
-copy /y %all_path%\baselib\build\Debug\sqlite3.lib %all_path%
+echo ****Note: copy thirdparty start****
+copy /y %all_path%\baselib\build\Debug\sqlite3.lib %all_path%\sqlite_func\thirdparty
+copy /y %all_path%\baselib\include\sqlite3.h %all_path%\sqlite_func\thirdparty
+echo ****Note: copy thirdparty done****
 echo ****Note: gen all done****
 cmd
